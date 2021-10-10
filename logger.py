8f150f76bf84dfa38475ca889dacb76e73be3aa7
Kostas Aznavouridis <kaznavouridis@gmail.com>
2021-10-10 21:30:20 +0300

    (D)

--- logger.py
+++ logger.py
@@ -1,66 +1,63 @@
 # -*- coding: utf-8 -*-
 from at.singleton import Singleton
+from colorama import init, Fore
+
+init(autoreset=True)
+
 
 class Logger(metaclass=Singleton):
     INFO = 'INFO'
     WARNING = 'WARNING'
     ERROR = 'ERROR'
+    GUI_EMPTY = '<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; color:#e7af06;"><br /></p>'
 
     color = {
         INFO: '#0D6EFD',
-        WARNING: '#FD7E14',
+        WARNING: '#E7AF06',
         ERROR: '#EF3E4F'
     }
 
-    def __init__(self, mode=None):
-        self._mode = mode
-        self._content = []
+    def __init__(self, mode='CLI'):
+        self.mode = mode
+        self.content = []
 
-    @staticmethod
-    def _display(content, kind=None):
-        if kind is None:
-            return content
-        else:
-            return f"[{kind}] - {content}"
+    def _add(self, content: str, modified: str):
+        if content.startswith('\n'):
+            self.content.append(self.GUI_EMPTY)
+        self.content.append(modified)
+        if content.endswith('\n'):
+            self.content.append(self.GUI_EMPTY)
 
     def set_mode(self, mode):
-        self._mode = mode
+        self.mode = mode
 
     def get_mode(self):
-        return self._mode
+        return self.mode
 
     def get_content(self):
-        to_show = '\n'.join(self._content)
+        to_show = ''.join(self.content)
         return to_show
 
-    def erase(self):
-        self._content = []
+    def clear(self):
+        self.content = []
 
-    def __call__(self, content=None, kind=None):
-        if self._mode == 'CLI':
-            if content is None:
-                print('\n')
-            else:
-                print(self._display(content, kind))
+    def info(self, content: str):
+        if self.mode == 'GUI':
+            _c = f'<p style="margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span">{content}</span></p>'
+            self._add(content=content, modified=_c)
         else:
-            if content is None:
-                _c = '<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p>'
-            else:
-                if kind is None:
-                    _c = f'<p style="margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">{content}</p>'
-                else:
-                    _c = f'<p style="margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">[<span style="color:{self.color[kind]};">{kind}</span>] - {content}</p>'
+            print(content)
 
-            self._content.append(_c)
+    def waring(self, content: str):
+        if self.mode == 'GUI':
+            _c = f'<p style="margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style="color:{self.color[self.WARNING]};">{content}</span></p>'
+            self._add(content=content, modified=_c)
+        else:
+            print(Fore.LIGHTYELLOW_EX + content)
 
-    def info(self, content=None):
-        if self.mode == 'CLI':
-            if content is None:
-                print('\n')
-            else:
-                print(Fore.WHITE + content)
-        elif self.mode == 'GUI':
-            _c = f'<p style="margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">[<span style="color:{self.color[self.INFO]};">{self.INFO}</span>] - {content}</p>'
-
-
-print(Style.BRIGHT + 'KOSTAS')
+    def error(self, content: str):
+        if self.mode == 'GUI':
+            _c = f'<p style="margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style="color:{self.color[self.ERROR]};">{content}</span></p>'
+            self._add(content=content, modified=_c)
+        else:
+            print(Fore.LIGHTRED_EX + content)
