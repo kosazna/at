dbcb8089eb422928ec141467ecf294af16c81a88
Kostas Aznavouridis <kaznavouridis@gmail.com>
2021-10-20 21:07:10 +0300

    (D)

--- utils.py
+++ utils.py
@@ -36,10 +36,10 @@ def load_user_settings(settings_file: Union[str, Path],
 
 def file_counter(src: Union[str, Path],
                  filters: Union[str, List[str], Tuple[str]],
-                 recursive: bool = False) -> dict:
+                 recursive: bool = True) -> dict:
     src_path = Path(src)
     file_filters = []
-
+    file_counter = {}
 
     if isinstance(filters, str):
         if recursive:
@@ -51,9 +51,17 @@ def file_counter(src: Union[str, Path],
             else:
                 file_filters.append(_filter)
 
-    file_counter = {}
-
     for file_filter in file_filters:
-            for p in src_path.glob(file_filter):
-                if p.is_dir():
-                    filename = p.stem
+        for p in src_path.glob(file_filter):
+            if p.is_file():
+                filename = p.stem
+                ext = p.suffix
+                if ext not in file_counter:
+                    file_counter[ext] = {}
+
+                if filename in file_counter[ext]:
+                    file_counter[ext][filename] += 1
+                else:
+                    file_counter[ext][filename] = 1
+
+    return file_counter
