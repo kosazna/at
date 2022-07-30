5b33f3d2227ba9d05fa33044a810eeae66aa0039
Kostas Aznavouridis <kaznavouridis@gmail.com>
2022-07-30 15:09:04 +0300

    (L) update gui init

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from at.gui.utils import validateParams, get_dpi, set_size
+from at.gui.utils import (validateParams, get_dpi, set_size,
+                          VERTICAL, HORIZONTAL, PATH_PLACEHOLDER)
 from at.gui.worker import run_thread
 from at.gui.components import *
