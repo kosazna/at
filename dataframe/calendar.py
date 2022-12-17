1173cefe16a183e56e14b4f8efa7a13aba99eb45
kosazna <kaznavouridis@gmail.com>
2022-12-17 14:07:45 +0200

    (D)

--- dataframe/calendar.py
+++ dataframe/calendar.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+from typing import Optional
 
-from pandas import Timestamp
+from pandas import Timestamp, date_range
 from pandas.tseries.holiday import (AbstractHolidayCalendar, Holiday,
                                     HolidayCalendarFactory)
 from pandas.tseries.offsets import CustomBusinessDay
@@ -33,20 +34,33 @@ class MoveableHolidaysGR(AbstractHolidayCalendar):
         Holiday(name="Megalo Savvato 2023", year=2023, month=4, day=15),
         Holiday(name="Kuriakh tou Pasxa 2023", year=2023, month=4, day=16),
         Holiday(name="Deutera tou Pasxa 2023", year=2023, month=4, day=17),
+        Holiday(name="Kathara Deutera 2024", year=2024, month=3, day=18),
+        Holiday(name="Megalh Paraskeuh 2024", year=2024, month=5, day=3),
+        Holiday(name="Megalo Savvato 2024", year=2024, month=5, day=4),
+        Holiday(name="Kuriakh tou Pasxa 2024", year=2024, month=5, day=5),
+        Holiday(name="Deutera tou Pasxa 2024", year=2024, month=5, day=6),
     ]
 
     start_date = Timestamp(datetime(2022, 1, 1))
-    end_date = Timestamp(datetime(2023, 12, 31))
+    end_date = Timestamp(datetime(2024, 12, 31))
+
+
+def get_business_days(year: int, custom_bday: Optional[CustomBusinessDay] = None):
+    start = datetime(year, 1, 1)
+    end = datetime(year, 12, 31)
+    drange = date_range(start, end, freq=custom_bday)
+
+    return drange
 
 
 BasicCalendarGR = BasicHolidaysGR(name='Basic Holidays')
 MoveableCalendarGR = MoveableHolidaysGR(name='Moveable Holidays for 2022-2023')
 
-HolidayCalendarGR_2022_2023 = HolidayCalendarFactory("Holidays 2022-2023",
+HolidayCalendarGR_2022_2024 = HolidayCalendarFactory("Holidays 2022-2024",
                                                      BasicCalendarGR,
                                                      MoveableCalendarGR)
 
-HolidaysGR = HolidayCalendarGR_2022_2023()
+HolidaysGR = HolidayCalendarGR_2022_2024()
 
 # to be used in "freq" parameter
 BusinessDaysGR = CustomBusinessDay(calendar=HolidaysGR)
