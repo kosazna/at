1e431d80651533f13809494b54766f3725acaf1d
kosazna <kaznavouridis@gmail.com>
2022-12-17 13:44:45 +0200

    (D)

--- dataframe/calendar.py
+++ dataframe/calendar.py
@@ -8,7 +8,7 @@ from pandas.tseries.holiday import (AbstractHolidayCalendar, Holiday,
 from pandas.tseries.offsets import CustomBusinessDay
 
 
-class GreeceHolidayCalendarBase(AbstractHolidayCalendar):
+class BasicHolidaysGR(AbstractHolidayCalendar):
     rules = [
         Holiday(name="New Year's Eve", month=1, day=1),
         Holiday(name="Theofaneia", month=1, day=6),
@@ -21,7 +21,7 @@ class GreeceHolidayCalendarBase(AbstractHolidayCalendar):
     ]
 
 
-class GreeceMoveableHolidayCalendar(AbstractHolidayCalendar):
+class MoveableHolidaysGR(AbstractHolidayCalendar):
     rules = [
         Holiday(name="Kathara Deutera 2022", year=2022, month=3, day=7),
         Holiday(name="Megalh Paraskeuh 2022", year=2022, month=4, day=22),
@@ -39,13 +39,14 @@ class GreeceMoveableHolidayCalendar(AbstractHolidayCalendar):
     end_date = Timestamp(datetime(2023, 12, 31))
 
 
-BasicHolidaysGR = GreeceHolidayCalendarBase(name='Basic Greece Holidays')
-MoveableHolidays_2022_2023 = GreeceMoveableHolidayCalendar(
-    name='Greece Holidays for 2022-2023')
+BasicCalendarGR = BasicHolidaysGR(name='Basic Holidays')
+MoveableCalendarGR = MoveableHolidaysGR(name='Moveable Holidays for 2022-2023')
 
-HolidayCalendarGR_2022_2023 = HolidayCalendarFactory(
-    "Greece Holidays 2022-2023", BasicHolidaysGR, MoveableHolidays_2022_2023)
+HolidayCalendarGR_2022_2023 = HolidayCalendarFactory("Holidays 2022-2023",
+                                                     BasicCalendarGR,
+                                                     MoveableCalendarGR)
 
 HolidaysGR = HolidayCalendarGR_2022_2023()
 
-BusinessDaysGR = CustomBusinessDay(calendar=HolidaysGR) # to be used in "freq" parameter
+# to be used in "freq" parameter
+BusinessDaysGR = CustomBusinessDay(calendar=HolidaysGR)
